4c009c5fbf940ede40ad9fc8e43ac2e7597e917b
OmarGomaaFCi <omargomaafcitu@gmail.com>
2023-07-06 06:04:13 +0200

    Create Two Factory methods for CustomResponse for handling Success and Failure

--- Movies.Core/Models/CustomResponse.cs
+++ Movies.Core/Models/CustomResponse.cs
@@ -1,8 +1,8 @@
 ﻿namespace Movies.Core.Models
 {
-	public class CustomResponse<T> 
+	public class CustomResponse<T>
 	{
-		public bool Status { get; set; } = true;	
+		public bool Status { get; set; } = true;
 		public int StatusCode { get; set; }
 
 		public string Message { get; set; }
@@ -10,5 +10,31 @@
 		public T Data { get; set; }
 
 		public List<string>? Errors { get; set; }
+
+		static CustomResponse<object> CreateFailureCustomResponse(int statusCode, List<string> errors)
+			=> new()
+			{
+				Status = false,
+				StatusCode = statusCode,
+				Message = ResponseConstants.MessageFailure,
+				Errors = errors
+			};
+
+
+		static CustomResponse<T> CreateSuccessCustomResponse(int statusCode, T data)
+			=> new()
+			{
+				Data = data,
+				StatusCode = statusCode,
+				Message = ResponseConstants.MessageSuccess,
+				Errors = { },
+				Status = true
+			};
+	}
+
+	public static class ResponseConstants
+	{
+		public const string MessageSuccess = "Success";
+		public const string MessageFailure = "Failed";
 	}
 }
