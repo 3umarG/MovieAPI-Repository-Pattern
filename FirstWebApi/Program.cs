ca704020fc5530a17ba3f74446ae61c66c669815
OmarGomaaFCi <omargomaafcitu@gmail.com>
2023-07-09 01:11:28 +0200

    Apply AutoMapper for the Project

--- FirstWebApi/Program.cs
+++ FirstWebApi/Program.cs
@@ -12,6 +12,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddCors();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 builder.Services.AddDbContext<ApplicationDbContext>(
 	options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"),
---
+++ Movies.Core/MappingProfile.cs
@@ -0,0 +1,52 @@
+﻿using AutoMapper;
+using Movies.Core.DTOs;
+using Movies.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Movies.Core
+{
+	public class MappingProfile : Profile
+	{
+        public MappingProfile()
+        {
+			//CreateMap<Tsource, Tdestination>();
+
+			/*
+             CharacterResponseDto
+
+            public int Id { get; set; }
+
+			public string FirstName { get; set; }
+
+			public string? LastName { get; set; }
+
+			public DateTime BirthDate { get; set; }
+             */
+
+			/*
+			Character
+
+				public int ID { get; set; }
+
+				public Name CharacterName { get; set; }
+
+				public DateTime BirthDate { get; set; }
+
+				public virtual ICollection<CharacterInMovie> CharacterActInMovies { get; set; } = new HashSet<CharacterInMovie>();
+
+			 */
+
+
+			CreateMap<Character, CharacterResponseDto>()
+				.ForMember(dest => dest.Id , src => src.MapFrom(src => src.ID)) // Map the Ids
+				.ForMember(des => des.FirstName , src => src.MapFrom(src => src.CharacterName.FirstName))
+				.ForMember(des => des.LastName , src => src.MapFrom(src => src.CharacterName.LastName))
+				
+				;
+		}
+	}
+}
