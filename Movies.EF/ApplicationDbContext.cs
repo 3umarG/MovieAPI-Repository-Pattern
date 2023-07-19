bb2bd8bc71d390158cb0f0863fee329383a63330
OmarGomaaFCi <omargomaafcitu@gmail.com>
2023-07-19 12:21:00 +0200

    Modify ApplicationDbContext for EF as IdentityDbContext

--- Movies.EF/ApplicationDbContext.cs
+++ Movies.EF/ApplicationDbContext.cs
@@ -3,10 +3,13 @@
 using Movies.EF.Config;
 using Microsoft.EntityFrameworkCore;
 using Movies.Core.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Movies.Core.Models.Auth;
+using Microsoft.AspNetCore.Identity;
 
 namespace FirstWebApi.Models
 {
-	public class ApplicationDbContext : DbContext
+	public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
 	{
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
         {
@@ -17,6 +20,28 @@ namespace FirstWebApi.Models
 
 		protected override void OnModelCreating(ModelBuilder modelBuilder)
 		{
+			base.OnModelCreating(modelBuilder);
+
+
+			modelBuilder.Entity<ApplicationUser>().ToTable("Users");
+			modelBuilder.Entity<IdentityRole>().ToTable("Roles");
+			modelBuilder.Entity<IdentityUserRole<string>>().ToTable("UserRoles");
+			modelBuilder.Entity<IdentityUserLogin<string>>().ToTable("UserLogins");
+			modelBuilder.Entity<IdentityUserToken<string>>().ToTable("UserTokens");
+			modelBuilder.Entity<IdentityUserClaim<string>>().ToTable("UserClaims");
+			modelBuilder.Entity<IdentityRoleClaim<string>>().ToTable("RoleClaims");
+
+			modelBuilder.Entity<ApplicationUser>()
+				.OwnsMany(
+				u => u.RefreshTokens,
+				t =>
+				{
+					t.WithOwner().HasForeignKey("UserId");
+					t.Property<int>("Id");
+					t.HasKey("Id");
+				});
+			
+
 			modelBuilder.ApplyConfiguration(new GenreConfig());
 			modelBuilder.ApplyConfiguration(new MovieConfig());
 			modelBuilder.ApplyConfiguration(new CharcaterConfig());
